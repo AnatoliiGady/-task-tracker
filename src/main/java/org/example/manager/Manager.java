3a2a91fc783366b298aac752a739475345b1ee5c
AnatoliiGady <grek-prost@yandex.ru>
2023-10-13 19:34:45 +0900

    Реализовал класс Manager

--- src/main/java/org/example/manager/Manager.java
+++ src/main/java/org/example/manager/Manager.java
@@ -1,120 +1,60 @@
 package org.example.manager;
 
 import org.example.InMemoryTaskDao;
-import org.example.taskDao.TaskDao;
 import org.example.task.Epic;
 import org.example.task.SubTask;
 import org.example.task.Task;
+import org.example.taskDao.TaskDao;
 
-import java.util.Map;
+public class Manager {
 
-public class Manager implements TaskDao {
+    private final TaskDao taskDao = new InMemoryTaskDao();
 
-    private final InMemoryTaskDao memoryTaskDao = new InMemoryTaskDao();
-
-    @Override
     public int add(Task task) {
-        memoryTaskDao.getTasks().put(task.getId(), task);
-        return task.getId();
+        int id = taskDao.add(task);
+        return id;
     }
 
-    @Override
     public int add(Epic epic) {
-        memoryTaskDao.getEpics().put(epic.getId(), epic);
-        return epic.getId();
+        int id = taskDao.add(epic);
+        return id;
     }
 
-    @Override
     public int add(SubTask subTask) {
-        memoryTaskDao.getSubTasks().put(subTask.getId(), subTask);
-        return subTask.getId();
+        int id = taskDao.add(subTask);
+        return id;
     }
 
-    @Override
-    public boolean removeTaskById(Task id) {
-        boolean rsl = false;
-        for (Map.Entry<Integer, Task> task : memoryTaskDao.getTasks().entrySet()) {
-            if (task.getKey() == id.getId()) {
-                memoryTaskDao.getTasks().remove(id.getId());
-                rsl = true;
-            }
-        }
-        return rsl;
+    public boolean removeTaskById(int id) {
+        taskDao.removeTaskById(id);
+        return true;
     }
 
-    @Override
-    public boolean removeEpicById(Epic id) {
-        boolean rsl = false;
-        for (Map.Entry<Integer, Epic> epic : memoryTaskDao.getEpics().entrySet()) {
-            if (epic.getKey() == id.getId()) {
-                memoryTaskDao.getEpics().remove(id.getId());
-                rsl = true;
-            }
-        }
-        return rsl;
+    public boolean removeEpicById(int id) {
+        taskDao.removeEpicById(id);
+        return true;
     }
 
-    @Override
-    public boolean removeSubTaskById(SubTask id) {
-        boolean rsl = false;
-        for (Map.Entry<Integer, SubTask> subTask : memoryTaskDao.getSubTasks().entrySet()) {
-            if (subTask.getKey() == id.getId()) {
-                memoryTaskDao.getSubTasks().remove(id.getId());
-                rsl = true;
-            }
-        }
-        return rsl;
+    public boolean removeSubTask(int id) {
+        taskDao.removeSubTaskById(id);
+        return true;
     }
-
-    @Override
     public Task getTaskById(int id) {
-        for (Map.Entry<Integer, Task> task : memoryTaskDao.getTasks().entrySet()) {
-            if (task.getKey() == id) {
-                return memoryTaskDao.getTasks().get(id);
-            }
-        }
-        return null;
+        return taskDao.getTaskById(id);
     }
-
-    @Override
     public Epic getEpicById(int id) {
-        for (Map.Entry<Integer, Epic> epic : memoryTaskDao.getEpics().entrySet()) {
-            if (epic.getKey() == id) {
-                return memoryTaskDao.getEpics().get(id);
-            }
-        }
-        return null;
+        return taskDao.getEpicById(id);
     }
-
-    @Override
     public SubTask getSubTaskById(int id) {
-        for (Map.Entry<Integer, SubTask> subTask : memoryTaskDao.getSubTasks().entrySet()) {
-            if (subTask.getKey() == id) {
-                return memoryTaskDao.getSubTasks().get(id);
-            }
-        }
-        return null;
+        return taskDao.getSubTaskById(id);
     }
-
-    @Override
     public void update(Task task) {
-        memoryTaskDao.getTasks().put(task.getId(), task);
+        taskDao.update(task);
     }
-
-    @Override
     public void update(Epic epic) {
-        memoryTaskDao.getEpics().put(epic.getId(), epic);
+        taskDao.update(epic);
     }
-
-    @Override
     public void update(SubTask subTask) {
-        memoryTaskDao.getSubTasks().put(subTask.getId(), subTask);
-    }
-
-    @Override
-    public String toString() {
-        return "Manager{"
-                + "memoryTaskDao=" + memoryTaskDao
-                + '}';
+        taskDao.update(subTask);
     }
 }
